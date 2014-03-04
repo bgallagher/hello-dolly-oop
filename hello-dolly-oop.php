5a00c1042d85f5b403a8833601973e72799a32e4
Brian Gallagher <gallagher.brian@gmail.com>
2014-03-04 15:28:48 +0000

    adding plugin file

---
+++ hello-dolly-oop.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: Hello Dolly OOP
+ * Plugin URI: https://github.com/bgallagher/hello-dolly-oop
+ * Description: An Object Oriented Version of Hello Dolly
+ * Version: 1.0
+ * Author: Brian Gallagher
+ * Author URI: http://briangallagher.ie
+ */
